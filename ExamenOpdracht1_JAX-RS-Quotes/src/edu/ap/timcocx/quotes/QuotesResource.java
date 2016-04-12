8fc677f5266607f8a6945dc55786b48103862734
Tim <timcocx788@gmail.com>
2016-04-12 16:57:50 +0200

    Opvul code toegevoegd + de POST methode toegevoegd

--- ExamenOpdracht1_JAX-RS-Quotes/src/edu/ap/timcocx/quotes/QuotesResource.java
+++ ExamenOpdracht1_JAX-RS-Quotes/src/edu/ap/timcocx/quotes/QuotesResource.java
@@ -6,18 +6,22 @@ import redis.clients.jedis.Jedis;
 
 @Path("/quotes")
 public class QuotesResource {
-	
+
+	// Elke quote die er is
 	@GET
-	@Produces({"text/html"})
+	@Produces({ "text/html" })
 	public String getAllQuotes() {
+		// Uncomment voor opvullen
+		//this.fillDb(true);
+
 		Jedis jedis = JedisConnection.getInstance().getConnection();
 		StringBuilder builder = new StringBuilder();
-		
+
 		builder.append("<html>");
 		builder.append("<head>");
 		builder.append("<title>Quotes</title>");
 		builder.append("</head>");
-		
+
 		builder.append("<body>");
 		builder.append("<ul>");
 		for (String key : jedis.keys("quotes:*")) {
@@ -28,7 +32,64 @@ public class QuotesResource {
 		builder.append("</ul>");
 		builder.append("</body>");
 		builder.append("</html>");
-		
+
 		return builder.toString();
+	}
+
+	// Specifieke auteur
+	// Data => de data die wordt gepost
+	@POST
+	public String getAuthorQuotes(String data) {
+		// Uncomment voor opvullen
+		//this.fillDb(true);
+
+		Jedis jedis = JedisConnection.getInstance().getConnection();
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("<html>");
+		builder.append("<head>");
+		builder.append("<title>Quotes</title>");
+		builder.append("</head>");
+
+		builder.append("<body>");
+		// Selecteren van correcte author
+		for (String author : jedis.keys("author:*")) {
+			String tmpAuthor = jedis.get(author);
+			if (tmpAuthor.equals(data)) {
+				builder.append("testink");
+				int authorId = Integer.parseInt(author.split(":")[1]);
+
+				builder.append("<h1>" + tmpAuthor + "</h1>");
+				builder.append("<ul>");
+				for (String quote : jedis.smembers("quotes:" + authorId)) {
+					builder.append("<li>" + quote);
+				}
+				builder.append("</ul>");
+
+				break; // Breaken is normaal nooit een goed idee, maar het haalt
+						// performantie omhoog als je het doet in een foreach
+						// loop
+			}
+		}
+
+		builder.append("</body>");
+		builder.append("</html>");
+
+		return builder.toString();
+	}
+
+	private void fillDb(boolean flush) {
+		Jedis jedis = JedisConnection.getInstance().getConnection();
+
+		if (flush) {
+			jedis.flushDB();
+		}
+
+		jedis.set("author:1", "Einstein");
+		jedis.set("author:2", "Stephen Hawking");
+
+		jedis.sadd("quotes:1", "A person who never made a mistake never tried anything new");
+		jedis.sadd("quotes:1", "If you want to live a happy life, tie it to a goal, not to people or objects");
+		jedis.sadd("quotes:2", "People who boast about their IQ are losers");
 	}
 }
